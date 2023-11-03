1ca4107d0ca5a1b0b67c46d705601ec1df317600
JEBA <fawjiajeba@gmail.com>
2023-11-03 10:27:14 +0600

    project initialize

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Gallery from './components/Gallery';
+import './components/styles.css';
+function App() {
+  return (
+    <div className="App">
+     <div className="App">
+        <h1 style={{color:"lightblue"}}>Image Gallery</h1>
+      <Gallery />
+    </div>
+    </div>
+  );
+}
+
+export default App;
